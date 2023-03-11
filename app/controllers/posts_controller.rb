cd130fa736fa4e17bd2485695a806c1bedbd6049
surajsubrahmanyankellogg <suraj.subrahmanyan@kellogg.northwestern.edu>
2023-03-11 16:02:37 +0000

    Updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post.save
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        redirect_to "/places"
+      else
+        flash["notice"] = "Try different login credentials!"
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "No!"
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "See you soon!"
+    redirect_to "/login"
   end
 end
-  
